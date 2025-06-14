5d74992f38f34d1ee8d72082f85f859c23e133f2
arkajyoti-roy <arkajyotiroy445@gmail.com>
2025-06-14 20:11:49 +0530

    push

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: '*',
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
